9bce24665debe8fce43f61a5abf1302b503454a5
Daniel Vianna <dmlvianna@gmail.com>
2017-12-18 15:01:21 +1100

    working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # framecsv
+
+95% shamelessly copied from a chat message by @idontgetoutmuch. This is just a simple example workflow from a CSV file to a PNG plot.
+
+Building it will not create the plot. After building, one has to issue `$ stack exec -- framescv -o "out.png"` to get a file containing the plot.
+
+Yes, I could have found a better CSV to plot with.
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,29 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, TemplateHaskell #-}
+
 module Main where
+
+import Diagrams.Prelude
+import Diagrams.Backend.Rasterific.CmdLine
+import Plots
+
+import Data.Foldable
+import Frames
+
+
+tableTypes "Row" "src/iris.csv"
+
+loadRows :: IO (Frame Row)
+loadRows = inCoreAoS (readTable "src/iris.csv")
+
+ukAxis :: Frame Row -> Axis B V2 Double
+ukAxis rs = r2Axis &~ do
+  let ys = toList (view sepalWidth <$> rs)
+      gs = toList (view sepalLength <$> rs)
+  linePlot' $ zip ys gs
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  rs <- loadRows
+  r2AxisMain $ ukAxis rs
+  return ()
